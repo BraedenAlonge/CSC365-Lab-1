1d0ee410f7d8c771313e013fcf9bcd7a182a9845
Miro Haapalainen <miro.haapalainen@gmail.com>
2024-09-26 14:39:42 -0700

    Implement student command

--- Database.java
+++ Database.java
@@ -1,12 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
 
 public class Database {
-
     public static ArrayList<String[]> getGradeClassroomTeacher(String lastname) {
         File datafile = new File("students.txt");
         ArrayList<String[]> result = new ArrayList<>();
@@ -15,12 +15,14 @@ public class Database {
             while (fileScanner.hasNext()) {
                 String data = fileScanner.nextLine();
                 String[] splitData = data.split(",");
-                String[] current = new String[3];
-                current[0] = splitData[2];
-                current[1] = splitData[3];
-                String teacherName = splitData[6] + " " + splitData[7];
-                current[2] = teacherName;
-                result.add(current);
+                if (splitData[0].equalsIgnoreCase(lastname)) {
+                    String[] current = new String[3];
+                    current[0] = splitData[2];
+                    current[1] = splitData[3];
+                    String teacherName = splitData[6] + " " + splitData[7];
+                    current[2] = teacherName;
+                    result.add(current);
+                }
             }
             return result;
         } catch (FileNotFoundException ex) {
@@ -51,7 +53,6 @@ public class Database {
 
 
     public static void main(String[] args) {
-
         Scanner inputScanner = new Scanner(System.in);
         String inputString = "";
         while (true) {
@@ -59,17 +60,18 @@ public class Database {
             inputString = inputScanner.nextLine();
             String[] splitStr = inputString.split("\\s");
             String cmd = splitStr[0];
-            switch(cmd) {
+            switch (cmd) {
                 // Quit command
                 case "Q":
                 case "Quit":
                     break;
-                    // Info command
+                // Info command
                 case "I":
                 case "Info":
                     for (int i = 0; i < 7; i++) {
                         System.out.println("Students in grade" + i + ": " + totalNumberOfStudentsInGrade(i));
                     }
+                    continue;
                     // Student command
                 case "S":
                 case "Student":
@@ -77,134 +79,60 @@ public class Database {
                         System.out.println("Invalid Parameters: Student last name required.");
                         continue;
                     }
-
                     if (splitStr.length == 2) {
                         ArrayList<String[]> result = getGradeClassroomTeacher(splitStr[1]);
+                        assert result != null;
                         for (String[] student : result) {
-                            System.out.println("Student last name: ");
+                            System.out.println("Student last name: " + splitStr[1]);
+                            System.out.println("Student grade: " + student[0]);
+                            System.out.println("Student classroom: " + student[1]);
+                            System.out.println("Student teacher: " + student[2]);
+                            System.out.println();
                         }
                     }
                     //If Bus Number is included
                     else if (splitStr.length == 3) {
-
                         int busNo = Integer.parseInt(splitStr[2]);
-                        System.out.println("Bussy");
+                        System.out.println("Bussy");    // Braeden what is this lmao
                         if (busNo == 0) {
                             System.out.println("Invalid Bus Number.");
-                        }}
-                    else { //Call Function with bus number included
-
+                        }
+                    } else { //Call Function with bus number included
                         System.out.println("Error - invalid number of arguments");
                     }
+                    continue;
                 case "T":
                 case "Teacher":
                     if (splitStr[1] == null) {
                         System.out.println("Invalid Parameters: Student last name required.");
                         continue;
                     }
-                //Bus Option
-                else if (splitStr[0].equals("B") || splitStr[0].equals("Bus")) {
-                    if (splitStr[1] == null) {
-                        System.out.println("Invalid Parameters: Bus number required.");
-                        continue;
+                    //Bus Option
+                    else if (splitStr[0].equals("B") || splitStr[0].equals("Bus")) {
+                        if (splitStr[1] == null) {
+                            System.out.println("Invalid Parameters: Bus number required.");
+                            continue;
+                        }
                     }
-                }
-                //Grade Option
-                else if (splitStr[0].equals("G") || splitStr[0].equals("Grade")) {
-                    if (splitStr[1] == null) {
-                        System.out.println("Invalid Parameters: Grade required.");
-                        continue;
+                    //Grade Option
+                    else if (splitStr[0].equals("G") || splitStr[0].equals("Grade")) {
+                        if (splitStr[1] == null) {
+                            System.out.println("Invalid Parameters: Grade required.");
+                            continue;
+                        }
                     }
-                }
-                //Average Option
-                else if (splitStr[0].equals("A") || splitStr[0].equals("Average")) {
-                    if (splitStr[1] == null) {
-                        System.out.println("Invalid Parameters: Grade required.");
-                        continue;
+                    //Average Option
+                    else if (splitStr[0].equals("A") || splitStr[0].equals("Average")) {
+                        if (splitStr[1] == null) {
+                            System.out.println("Invalid Parameters: Grade required.");
+                            continue;
+                        }
                     }
-                }
-                    default:
+                    continue;
+                default:
                     System.out.println("Error - Invalid command");
             }
-            // Quit Option
-      /*      if (splitStr[0].equals("Q") || splitStr[0].equals("QUIT")) {
-                break;
-            }
-            //Info Option
-            else if (splitStr[0].equals("I") || splitStr[0].equals("Info")) {
-                for (int i = 0; i < 7; i++) {
-                    System.out.println("Students in grade" + i + ": " + totalNumberOfStudentsInGrade(i));
-                }
-
-
-            }
-            //Student Option
-              else if (splitStr[0].equals("S") || splitStr[0].equals("Student")) {
-                  if (splitStr.length == 1 || splitStr[1] == null) {
-                        System.out.println("Invalid Parameters: Student last name required.");
-                        continue;
-                    }
-
-                if (splitStr.length == 2) {
-                    ArrayList<String[]> result = getGradeClassroomTeacher(splitStr[1]);
-                    for (String[] student : result) {
-                        System.out.println("Student last name: ");
-                    }
-                }
-                //If Bus Number is included
-                else if (splitStr.length == 3) {
-
-                    int busNo = Integer.parseInt(splitStr[2]);
-                    System.out.println("Bussy");
-                    if (busNo == 0) {
-                        System.out.println("Invalid Bus Number.");
-                    }}
-                    else { //Call Function with bus number included
-
-                        System.out.println("Error - invalid number of arguments");
-                    }
-
-
-
-            }
-            //Teacher Option
-            else if (splitStr[0].equals("T") || splitStr[0].equals("Teacher")) {
-                if (splitStr[1] == null) {
-                    System.out.println("Invalid Parameters: Student last name required.");
-                    continue;
-                }
-            }
-            //Bus Option
-            else if (splitStr[0].equals("B") || splitStr[0].equals("Bus")) {
-                if (splitStr[1] == null) {
-                    System.out.println("Invalid Parameters: Bus number required.");
-                    continue;
-                }
-            }
-            //Grade Option
-            else if (splitStr[0].equals("G") || splitStr[0].equals("Grade")) {
-                if (splitStr[1] == null) {
-                    System.out.println("Invalid Parameters: Grade required.");
-                    continue;
-                }
-            }
-            //Average Option
-            else if (splitStr[0].equals("A") || splitStr[0].equals("Average")) {
-                if (splitStr[1] == null) {
-                    System.out.println("Invalid Parameters: Grade required.");
-                    continue;
-                }
-
-            }
-            //Invalid Operands Option
-            else   {
-                System.out.println("Invalid Option. ");
-
-            } */
-
         }
-
-
     }
 }
 
