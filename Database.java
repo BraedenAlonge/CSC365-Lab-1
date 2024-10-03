6b2d7791ee5cc7deec1ae8f036a3a9515524858e
Miro Haapalainen <miro.haapalainen@gmail.com>
2024-10-02 19:09:23 -0700

    Refactor some more thangs and shiii

--- Database.java
+++ Database.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Database {
@@ -24,15 +26,63 @@ public class Database {
         Info
     }
 
-    public String readFromDatabase(Command c) {
+    public String readFromDatabase(Command c, ArrayList<Object> args) {
         try {
             Scanner teacherScanner = new Scanner(teachersFile);
             Scanner listScanner = new Scanner(listFile);
-
+            switch(c) {
+                case Student: {
+                    String lastname = (String) args.get(0);
+                    return StudentCmd(listScanner, lastname);
+                }
+                case StudentBus: {
+                    String lastname = (String) args.get(0);
+                    return StudentBusCmd(listScanner, lastname);
+                }
+                case Bus: {
+                    String busNum = (String) args.get(0);
+                    return BusCmd(listScanner, busNum);
+                }
+                case Teacher: {
+                    String teacher = (String) args.get(0);
+                    return TeacherCmd(teacherScanner, teacher);
+                }
+                case Average: {
+                    ;
+                }
+                case Grade: {
+                    ; }
+                case Info: {
+                    ; }
+            }
     } catch (FileNotFoundException e) {
         System.out.println("Error opening file");
         System.exit(0);
         }
+        return null;
+    }
+
+    public String StudentCmd(Scanner listScanner, String lastname) {
+        return null;
+    }
+
+    public String StudentBusCmd(Scanner listScanner, String lastname) {
+        return null;
+    }
+
+    public String BusCmd(Scanner listScanner, String busNum) {
+        return null;
+    }
+
+    public String TeacherCmd(Scanner teacherScanner, String teacher) {
+        return null;
+    }
+
+    public String Average(Scanner listScanner, String grade) {
+        return null;
+    }
+
+    public String Grade(Scanner listScanner, String grade, int option) {
         return null;
     }
 }
